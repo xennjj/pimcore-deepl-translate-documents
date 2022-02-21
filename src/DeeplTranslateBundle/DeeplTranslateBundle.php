d35d2fdf049afe6bd3af884c0302cf3f05437bec
Thomas Drews <t.drews@agorate.de>
2022-02-21 13:21:37 +0100

    First commit

---
+++ src/DeeplTranslateBundle/DeeplTranslateBundle.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace agorate\DeeplTranslationBundle;
+
+use PackageVersions\Versions;
+use Pimcore\Extension\Bundle\AbstractPimcoreBundle;
+use Pimcore\Extension\Bundle\Traits\PackageVersionTrait;
+
+class agorateDeeplTranslationBundle extends AbstractPimcoreBundle
+{
+    use PackageVersionTrait;
+    const PACKAGE_NAME = 'agorate/pimcore-deepl-translate-documents';
+
+    /**
+     * Returns all used JavaScript files
+     *
+     * @return string[]
+     */
+    public function getJsPaths(): array
+    {
+        return [
+            '/bundles/DeeplTranslateBundle/js/deepl-translation/startup.js'
+        ];
+
+    }
+
+    /**
+     * Bundle name as shown in extension manager
+     *
+     * @return string
+     */
+    public function getNiceName(): string
+    {
+        return 'Agorate - Deepl Translate Document Pimcore Bundle';
+    }
+
+    /**
+     * Bundle description as shown in extension manager
+     *
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return "";
+    }
+
+    /** normalizes version to pretty version
+     * e. g. v2.3.0@9e016f4898c464f5c895c17993416c551f1697d3 to 2.3.0
+     *
+     * @return array|string|null
+     */
+    public static function getSolutionVersion(): array|string|null
+    {
+        $version = Versions::getVersion(self::PACKAGE_NAME);
+
+        $version = preg_replace('/^v/', '', $version);
+        return preg_replace('/@(.+)$/', '', $version);
+    }
+
+    /**
+     * Returns the composer package name used to resolve the version
+     *
+     * @return string
+     */
+    protected function getComposerPackageName(): string
+    {
+        return self::PACKAGE_NAME;
+    }
+}
---
+++ src/DeeplTranslateBundle/Service/DeeplService.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Agorate\PimcoreDeeplTranslateDocuments\DeeplTranslateBundle\Service;
+
+use Pimcore\Model\WebsiteSetting;
+use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class DeeplService
+{
+    private HttpClientInterface $httpClient;
+
+    public function __construct()
+    {
+        $this->httpClient = HttpClient::create();
+    }
+
+    /**
+     * @throws TransportExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ClientExceptionInterface
+     */
+    public function translate(?string $text, string $targetLocale): ?string
+    {
+        if (is_null($text)) {
+            return null;
+        }
+
+        $authKey = WebsiteSetting::getByName("deepl_auth_key") ? WebsiteSetting::getByName("deepl_auth_key")->getData() : null;
+
+        $response = $this->httpClient->request('POST', "https://api.deepl.com/v2/translate", [
+            'body' => [
+                'auth_key' => $authKey,
+                'text' => $text,
+                'target_lang' => substr($targetLocale, 0, 2)
+            ]
+        ]);
+
+        $parsedResponse = json_decode($response->getContent());
+
+        return $parsedResponse->translations[0]->text;
+    }
+}
